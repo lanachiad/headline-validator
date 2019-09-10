55942272b7962bbb3f3f2a01985553a0a1f48486
lchiad <lchiad@webmd.net>
2019-09-10 16:12:37 -0400

    Initial commit

---
+++ src/Result.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Result extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			language: ''
+		}
+	}
+
+	render() {
+		return (
+			<h2>test</h2>
+
+		)
+	}
+}
+
+export default Result;

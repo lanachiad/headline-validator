f83aa6a42dd96e486f83af75b19ac9db71cc697e
lchiad <lchiad@webmd.net>
2019-09-10 16:15:47 -0400

    Add ideas for what Headline is supposed to do

--- src/Headline.js
+++ src/Headline.js
@@ -11,6 +11,13 @@ class Headline extends Component {
 	render() {
 		return (
 			<p>{this.state.text}</p>
+
+			// This will eventually store each headline copy passed through
+			// I'll create a short-term list of all headline copy inputs so that the user knows what attempts they've done
+			// There will also be a check or X next to this input copy to remind the user whether the headline was good to use or not
+			// We can also put the reasoning for why there's an X (this copy was branded/too long)
+
+			
 		)
 	}
 }

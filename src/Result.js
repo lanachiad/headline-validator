3a76aa2718ee2d3bef9be3f633b7dc8c09c421da
lchiad <lchiad@webmd.net>
2019-09-10 16:13:43 -0400

    Added ideas for what Result is supposed to do

--- src/Result.js
+++ src/Result.js
@@ -12,6 +12,9 @@ class Result extends Component {
 		return (
 			<h2>test</h2>
 
+			// This will handle what result message will come up to the user based on the headline validator's results
+			// So if this is branded, it will let the user know that there's branded language in their input
+			// If it's long, it will let the user know that they need to shorten the character count in their input
 		)
 	}
 }
